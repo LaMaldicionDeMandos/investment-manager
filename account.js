c71fb1859376b483d4e0fa5f22d3281a2a689129
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-10-23 14:29:25 -0300

    implemento el modulo que crea accounts desde archivo

---
+++ account.js
@@ -0,0 +1,22 @@
+var q = require('q');
+var Converter = require("csvtojson").Converter;
+var converter = new Converter({delimiter: ';'});
+
+var create = function(filename, name) {
+	var defer = q.defer();
+	var account = {name: name};
+	converter.on("end_parsed", function (jsonArray) {
+		jsonArray.forEach(function(item) {
+	   		item.opening = parseFloat(item.opening.replace(',', '.', 'gi'));
+   			item.close = parseFloat(item.close.replace(',', '.', 'gi'));
+   			item.percent = function() {
+			return 100*(item.close - item.opening)/item.opening;
+		};
+		account.history = jsonArray;
+		defer.resolve(account);
+   	}); 
+	}
+	return defer.promise;
+}
+
+module.exports = create;
