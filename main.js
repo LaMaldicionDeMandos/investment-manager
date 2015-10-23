213b19b4fea852bb94ab713a7632e787ef03b6f4
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-10-22 22:50:30 -0300

    primera aproximacion de la mejor aproximacion

--- main.js
+++ main.js
@@ -1,12 +1,42 @@
 var csv = require('csv-to-json');
 console.log('Path file: ' + process.argv[2]);
 var file = {filename: process.argv[2]};
+var size = process.argv[3] || 15;
 var Converter = require("csvtojson").Converter;
 var converter = new Converter({delimiter: ';'});
- 
+
 //end_parsed will be emitted once parsing finished 
 converter.on("end_parsed", function (jsonArray) {
-   console.log(jsonArray); //here is your result jsonarray 
+   	jsonArray.forEach(function(item) {
+   		item.opening = parseFloat(item.opening.replace(',', '.', 'gi'));
+   		item.close = parseFloat(item.close.replace(',', '.', 'gi'));
+   		item.percent = function() {
+			return 100*(item.close - item.opening)/item.opening;
+		};
+   	}); 
+   	var lasts = jsonArray.slice(0, size);
+   	jsonArray.splice(0, size);
+   	var difs = [];
+   	for (var i = 0; i < jsonArray.length - size;i++) {
+   		var diff = 0;
+   		for (var j = 0; j < size; j++) {
+   			diff+= Math.abs(lasts[j].percent() - jsonArray[i + j].percent());
+   		}
+   		difs.push(diff);
+   	}
+   	var min = 10000000;
+   	var index = 0;
+   	for (var i = 0; i < difs.length; i++) {
+   		if (difs[i] < min) {
+   			min = difs[i];
+   			index = i;
+   		}
+   	}
+   	console.log('Min: ' + min);
+   	console.log('Index: ' + index);
+   	for(var i = 0; i < size ; i++) {
+   		console.log('referencia: ' + lasts[i].percent() + '  --  obtenido: ' + jsonArray[index + i].percent());
+   	}
 });
  
 //read from file 
