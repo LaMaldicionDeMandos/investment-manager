a731e7b12f25063ff89fcf31662bb83bc7ee69dc
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-10-23 20:18:17 -0300

    trabajo en codes, primera pagina de BCBA

---
+++ title-codes.js
@@ -0,0 +1,23 @@
+var stockExchange = {
+	BCBA: [
+	{id: '48',name: 'AGRO'},
+	{id: '17388',name: 'AULA'},
+	{id: '21729',name: 'APBR'},
+	{id: '21520',name: 'AUSO'},
+	{id: '302',name: 'BHIP'},
+	{id: '444',name: 'BMA'},
+	{id: '34089',name: 'BOLT'},
+	{id: '34746',name: 'BPAT'},
+	{id: '424',name: 'BRIO'},
+	{id: '23049',name: 'BRIO6'},
+	{id: '535',name: 'CADO'},
+	{id: '551',name: 'CAPU'},
+	{id: '20832',name: 'CARC'},
+	{id: '745',name: 'CECO2'},
+	{id: '758',name: 'CELU'},
+	{id: '773',name: 'CEPU'},
+	{id: '28443',name: 'CGPA2'},
+	{id: '1011',name: 'COLO'},
+	{id: '1016',name: 'COME'},
+	]
+}	
