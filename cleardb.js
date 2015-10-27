be8701d7a54ea19772c9265b25cea63f44b27030
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-10-27 17:58:31 -0300

    elimino archivos que ya no uso

--- cleardb.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by marcelo on 27/10/15.
- */
-var db = require('./database.js');
-db.clean(function(err) {
-    console.log('collection removed, errors: ' + err);
-    process.exit();
-});
