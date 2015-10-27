ae9ca552bcc2e3116fab6fd51f6f0cde19db651c
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-10-27 14:58:38 -0300

    cargo los csv ya modificados

--- data-fetcher.js
+++ data-fetcher.js
@@ -1,16 +1,25 @@
 var request = require('request');
 var fs = require('fs');
+var th = require('through2');
 var baseUrl = 'https://www.invertironline.com/Titulo/DescargarCotizacionesHistoricasTitulo?idTitulo=';
 
 function Fetcher() {
+	var that = this;
 	this.fetchTitle = function(filepath, titleId) {
-		request(baseUrl + titleId).pipe(fs.createWriteStream(filepath));
+		var transform = th(function(data, encoding, done) {
+			var string = data.toString();
+			string = string.replace(/,/g, '.');
+			string = string.replace(/;/g, ',');
+			this.push(string);
+			done();
+		});
+		request(baseUrl + titleId).pipe(transform).pipe(fs.createWriteStream(filepath));
 	};
 	this.fetchTitles = function(path, titles) {
 		titles.forEach(function(key, title) {
 			var finalPath = path + '/' + key + '/' + title.name + '.csv';
 			console.log('Fetching ' + title.name + ' in ' + finalPath);
-			this.fetchTitle(finalPath, title.id);
+			that.fetchTitle(finalPath, title.id);
 		});
 	};
 };	
--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/investment');
+var connection = mongoose.createConnection('mongodb://localhost/investment');
 console.log('Connecting to mongodb');
 
 var Schema = mongoose.Schema;
@@ -21,7 +21,7 @@ function DBSchema() {
 var schema = new DBSchema();
 
 process.on('exit', function() {
-	mongoose.disconnect();
+	connection.disconnect();
 });
 
 module.exports = schema;
