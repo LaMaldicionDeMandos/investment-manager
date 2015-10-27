952f13d71464ee87ae864b2551cb06f28e2ed0b5
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-10-27 17:15:14 -0300

    elimino elementos que no uso mas

--- new-csv-test.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by marcelo on 27/10/15.
- */
-var csv = require('csvjson');
-var path = process.argv[2];
-
-var values = csv.toObject(path).output;
-console.log(JSON.stringify(values));
