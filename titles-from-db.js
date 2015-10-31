371ce89cb793eb3210485a5b7cbb0e7c97df0499
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-10-30 21:02:19 -0300

    implemento consulta de history

--- titles-from-db.js
+++ titles-from-db.js
@@ -6,14 +6,16 @@ var Title = db.Title;
 
 var name = process.argv[2];
 var query = name ? {name: name} : {};
-Title.find(query, function(err, titles) {
+Title.find(query).select('_id name windowReports history').exec(function(err, titles) {
    if (err) {
        console.log(err);
        return;
    }
    titles.forEach(function(title) {
        console.log('Report ' + title.name + ': ' + JSON.stringify(title.windowReports));
+       Title.findOne(title).select('history').exec(function(err, history) {
+           console.log('History: ' + history.history);
+       });
        console.log('---------------------------------------------');
    });
-    process.exit();
 });
