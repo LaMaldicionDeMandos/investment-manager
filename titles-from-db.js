eb98a0062a4457c428716f3a43433f90db968119
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-11-02 10:39:35 -0300

    cambio en visualizacion de los titulos

--- titles-from-db.js
+++ titles-from-db.js
@@ -6,16 +6,18 @@ var Title = db.Title;
 
 var name = process.argv[2];
 var query = name ? {name: name} : {};
-Title.find(query).select('_id name windowReports history').exec(function(err, titles) {
+Title.find(query).select('name windowReports').exec(function(err, titles) {
    if (err) {
        console.log(err);
        return;
    }
    titles.forEach(function(title) {
-       console.log('Report ' + title.name + ': ' + JSON.stringify(title.windowReports));
-       Title.findOne(title).select('history').exec(function(err, history) {
-           console.log('History: ' + history.history);
-       });
+       console.log('Report ' + title.name + '--> Antes: ' + title.windowReports[0].report.predictionBefore.before + " -- Despues: "
+       + title.windowReports[0].report.predictionBefore.after + " -- Maximo Antes: " +
+           (title.windowReports[0].report.predictionBefore.before + title.windowReports[0].report.predictionBefore.positiveError) + ' -- Maximo Despues: ' +
+           (title.windowReports[0].report.predictionBefore.after + title.windowReports[0].report.predictionBefore.positiveError) + " -- Minimo Antes: " +
+           (title.windowReports[0].report.predictionBefore.before - title.windowReports[0].report.predictionBefore.negativeError) + ' -- Minimo Despues: ' +
+           (title.windowReports[0].report.predictionBefore.after - title.windowReports[0].report.predictionBefore.negativeError));
        console.log('---------------------------------------------');
    });
 });
