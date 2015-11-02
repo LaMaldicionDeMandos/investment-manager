4291c24d03043f7d306ce1fd29cbe837fa06d6c1
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-11-01 23:09:10 -0300

    le agrego titulo y icono a la app

--- app/main.js
+++ app/main.js
@@ -25,7 +25,7 @@ app.on('window-all-closed', function () {
 app.on('ready', function () {
 
     // Create the browser window.
-    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow = new BrowserWindow({ width: 800, height: 600, title: 'Investment Manager', icon: 'app/assets/logo.png' });
 
     // and load the index.html of the app.
     mainWindow.loadUrl('file://' + __dirname + '/index.html');
