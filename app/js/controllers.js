e25e435338c209bacfa4f50305fd5d393fc9f8a5
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-11-04 10:53:30 -0300

    agrego libreria para iconos y ordeno titulos por nombre

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,9 +5,13 @@
     'use strict';
     angular.module('app.controllers', [])
         .controller('predictionsController', function($scope, titlesService) {
+            var sortByName = function(a, b) {
+                return a.name > b.name ? 1 : -1;
+                };
             titlesService.all().then(
                 function(titles) {
                     $scope.titles = titles;
+                    $scope.titles.sort(sortByName);
             },  function(error) {
                     console.log(error);
                 }
