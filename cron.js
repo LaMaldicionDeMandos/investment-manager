702ab08f7180242db352e943d8efa12e48e425b6
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-11-19 13:51:02 -0300

    seteo los valores correctos, de 11 a 17 de lunes a viernes

--- cron.js
+++ cron.js
@@ -1,3 +1,24 @@
 /**
  * Created by boot on 11/19/15.
  */
+var CronJob = require('cron').CronJob;
+var loop;
+var start = new CronJob('0 11 * * 1-2-3-4-5', function() {
+    console.log('Run loop');
+    loop = loop || new CronJob('*/30 * * * * *', function() {
+        console.log('Corre cada 30 segundos');
+    }, function() {
+        console.log('stop loop');
+    });
+    loop.start();
+});
+
+start.start();
+
+var stop = new CronJob('0 17 * * 1-2-3-4-5', function() {
+    if (loop) {
+        console.log('stoping loop');
+        loop.stop();
+    }
+});
+stop.start();
