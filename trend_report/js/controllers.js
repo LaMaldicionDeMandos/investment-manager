5a3dbc9f30370fe68eadda92fecf7c0acd0422e4
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-12-18 16:20:19 -0300

    implemento ordenamiento de trend report

--- trend_report/js/controllers.js
+++ trend_report/js/controllers.js
@@ -6,12 +6,27 @@
     angular.module('app.controllers', [])
         .controller('mainController', function($scope, globalTitlesService, $mdSidenav) {
             $scope.titles = undefined;
+            $scope.sorterBy;
             $scope.open = function() {
                 $mdSidenav('report')
                     .toggle()
                     .then(function () {
                         console.log("toggle  is done");
                     });
+            };
+            var compareByName = function(a, b) {
+                return a.name > b.name ? 1 : -1;
+            };
+            var compareByUp = function(a, b) {
+                return a.upPercent < b.upPercent ? 1 : -1;
+            };
+            $scope.sortByName = function() {
+                $scope.sorterBy = 'name';
+                $scope.titles.sort(compareByName);
+            };
+            $scope.sortByUp = function() {
+                $scope.sorterBy = 'up';
+                $scope.titles.sort(compareByUp);
             };
             $scope.selectTitle = function(title) {
                 $scope.currentTitle = title;
